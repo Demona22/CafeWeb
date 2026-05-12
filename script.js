cc7e5d0c9edf114eb0751f20c1da06101a34dbaa
Demona22 <demokienkkna@gmail.com>
2026-05-12 21:43:06 +0700

    Lamxong

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
@@ -6,5 +7,41 @@ menuOpenButton.addEventListener("click", () => {
     document.body.classList.toggle("show-mobile-menu");
 });
 
-menuCloseButton.addEventListener("click", () => menuOpenButton.click
-());
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () =>menuOpenButton.click()); 
+});
+        //Close mobile menu when a nav link is clicked
+
+// Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+     768: {
+        slidesPerView: 2
+    },
+     1024: {
+        slidesPerView: 3
+    },
+}
+});
